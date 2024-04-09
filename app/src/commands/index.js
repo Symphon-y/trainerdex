93bd83ac730deb88bfe49ddac2cc384b1326d370
Travis <travisredden@outlook.com>
2024-04-09 09:16:47 -0500

    [UPDATE] - updated trainer commands to include multiple options for registering trainer to dex

--- app/src/commands/index.js
+++ app/src/commands/index.js
@@ -1,8 +1,8 @@
 import 'dotenv/config';
+import { ALL_SERVER_COMMANDS } from './serverCommands.js';
+import { ALL_TRAINER_COMMANDS } from './trainerCommands.js';
 import { InstallGlobalCommands } from '../utils/index.js';
-import { ALL_TRAINER_COMMANDS } from "./trainerCommands.js"
-import { ALL_SERVER_COMMANDS } from "./serverCommands.js"
 
-const ALL_COMMANDS = ALL_SERVER_COMMANDS.concat(ALL_TRAINER_COMMANDS)
+const ALL_COMMANDS = ALL_SERVER_COMMANDS.concat(ALL_TRAINER_COMMANDS);
 
 InstallGlobalCommands(process.env.DISCORD_APPLICATION_ID, ALL_COMMANDS);
--- app/src/commands/trainerCommands.js
+++ app/src/commands/trainerCommands.js
@@ -4,10 +4,22 @@ const REGISTER_TRAINER = {
   description: 'Register your Trainer Code to TrainérDex.',
   options: [
     {
-      type: 3, // Type 3 is "STRING" for text input
+      type: 3, // Type 3 is "STRING"
       name: 'trainercode',
       description: 'Your Pokémon GO Trainer Code',
-      required: true, // Make this field required
+      required: true,
+    },
+    {
+      type: 3, // Type 3 is "STRING"
+      name: 'serverusername',
+      description: 'Your Server Username',
+      required: true,
+    },
+    {
+      type: 3, // Type 3 is "STRING"
+      name: 'pogousername',
+      description: 'Your Pokémon GO Username',
+      required: false,
     },
   ],
   type: 1, // Type 1 is "CHAT_INPUT" for slash commands
